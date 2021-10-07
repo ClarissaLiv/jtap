5ea314738a7e45626fde8cfa266e93873961b6ca
clivings <clivings@ivt.baug.ethz.ch>
2021-10-07 15:47:01 +0200

    testingGitSetup

---
+++ src/main/java/clarissaTestNonsense/NonsenseGitTest.java
@@ -0,0 +1,6 @@
+package clarissaTestNonsense;
+
+public class NonsenseGitTest {
+	//this class exists only to see how eclipse interacts with our specially set up git repository
+	//which we set up using the command line
+}
